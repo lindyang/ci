ee9f059a7be007aebb2270b332418220d16fb1f3
lindyang <ridward@qq.com>
2019-06-27 18:38:53 +0800

    add JUnit

---
+++ test/test_widget.py
@@ -0,0 +1,22 @@
+from unittest import TestCase, main
+
+from proj.main import Widget
+
+
+class WidgetTestCase(TestCase):
+    def setUp(self):
+        self.widget = Widget()
+
+    def tearDown(self):
+        self.widget = None
+
+    def test_size(self):
+        self.assertEqual(self.widget.get_size(), (40, 40))
+
+    def test_Resize(self):
+        self.widget.resize(100, 100)
+        self.assertEqual(self.widget.get_size(), (100, 1100))
+
+
+if __name__ == '__main__':
+    main()
